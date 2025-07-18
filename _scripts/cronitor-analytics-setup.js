1eb8d7fc2dab8a4b1a47729dd841bd6c4727dc1e
ZeyiLeo <leo070310@163.com>
2025-07-18 20:14:11 +0800

    11

--- _scripts/cronitor-analytics-setup.js
+++ _scripts/cronitor-analytics-setup.js
@@ -2,7 +2,7 @@
 permalink: /assets/js/cronitor-analytics-setup.js
 
 
-window.cronitor =
+window.cronitor 
   window.cronitor ||
   function () {
     (window.cronitor.q = window.cronitor.q || []).push(arguments);
